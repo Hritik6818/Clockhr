cd0731a6d82075bc8c99af63a3ea6b1b23f02aa6
Hritik Shukla <102315313+Hritik6818@users.noreply.github.com>
2023-08-21 18:11:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function displayTime(){
+    let dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let session = document.getElementById('session');
+    
+
+    if(hrs >=12){
+        session.innerHTML = "PM";
+    }
+    else{
+        session.innerHTML = "AM";
+    }
+if(hrs > 12){
+    hrs = hrs - 12;
+}
+    
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 10);
